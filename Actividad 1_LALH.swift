c5382c49d0ad1a76b1c73ca511607fbf4e476d3d
LuisLeal24 <61062727+LuisLeal24@users.noreply.github.com>
2021-08-17 17:45:50 -0500

    Desarrollo de Actividad 1

---
+++ Actividad 1_LALH.swift
@@ -0,0 +1,21 @@
+import Foundation
+/*
+    @autors: Luis Leal
+    17/08/2021
+*/
+
+//Declaración de una variable de tipo String
+var v1 = "esto es Swift"
+
+//Declaración de una constante de tipo entera
+let c1 = 4 
+
+//Representa la asignación formal de un tipo de dato String.
+var cadena:String = String()
+
+//Concatenación con la variable v1 
+cadena = "Hola mundo " + v1
+
+//Imprime los resultados
+print("El contenido es: \(cadena)")
+print("El valor de constante es: \(c1)")
